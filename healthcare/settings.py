9c7577c5521fbdb556f7743880dc7def09966168
Ramees Vadakkan <rameesvadakkan@gmail.com>
2025-12-27 20:47:42 +0530

    database integration

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$6^gy1f91!3e563yq@05@z(78-+yej*a9%=zea%!3x!b2v(qli'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "healthcare-management-system-basic.onrender.com",
@@ -138,7 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
